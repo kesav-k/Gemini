e81c784f8d4722c1c6cd7db6480e0adf0bc3ddf9
kesav-k <kesavk345@gmail.com>
2025-10-20 22:48:31 +0530

    Initial commit of React project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+import ContextProvider from './context/Context'
+const App =() => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/> 
+      </>
+  )
+}
+
+export default App
