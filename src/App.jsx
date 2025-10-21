40995227799852b8d14dd0f18895dd1bf1fe9484
kesav-k <kesavk345@gmail.com>
2025-10-21 07:22:27 +0530

    Fix import path for Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from './Components/Sidebar/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
 import ContextProvider from './context/Context'
 const App =() => {
